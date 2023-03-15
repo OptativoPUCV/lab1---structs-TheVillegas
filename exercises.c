f8055bf51ce7f9fff12b483394cd8020008b7752
Replit user <>
2023-03-15 15:35:06 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -116,7 +116,7 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  if(v==NULL || i i < 0 || i > v->capacidad)
+  if(v==NULL || i  < 0 || i > v->capacidad)
     {
       puts("no se ingreso ningun dato o sobrepaso la capacidad de almacenamiento");
       return;
