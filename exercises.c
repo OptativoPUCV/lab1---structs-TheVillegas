2fef616046777b8e9c9212349254d2c3d8f72179
Replit user <>
2023-03-17 02:22:39 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -148,6 +148,12 @@ Función que suma los vectores `a` y `b` y
 actualiza el vector `c` con el resultado de la suma.
 */
 void sumaV(Vector * a, Vector * b, Vector * c) {
+  if(a == NULL || b == NULL || c == NULL)
+  {
+    puts("no hay datos para sumar");
+    return;
+  }
+  
 
 }
 
