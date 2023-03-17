c429eb40b602a3f38a63529690b1d3b44e583b53
Replit user <>
2023-03-17 13:02:55 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -171,6 +171,6 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
     {
       puts("no hay datos almacenados");
     }
-    c->datos[0] = (a1,a2) + (b1+b2);
+    c->datos[0] = a1 + a2 + b1+b2 ;
 
 }
